5dbb52d0f7188203c1d1c24122254b8c8a845d79
Hong Hur <hhur@rockefeller.edu>
2021-09-09 15:03:28 -0400

    Tuto 1_3 done

--- 1_3_intro_to_js/main.js
+++ 1_3_intro_to_js/main.js
@@ -1,1 +1,18 @@
-console.log('hello world');
+
+function updateCounterDP() {
+    const counter_dp = document.getElementById("counter-dp")
+    counter_dp.innerText = parseInt(counter_dp.innerText) + 1
+}
+
+function clearCounterDP() {
+    document.getElementById("counter-dp").innerText = 0
+}
+
+function updateCounterDPwithINPUT() {
+    let input_val = document.getElementById("num-input").value
+    if (isNaN(input_val)) {
+        window.alert(`Please enter number only.`);
+    } else {
+        document.getElementById("counter-dp").innerText = input_val
+    }
+}
